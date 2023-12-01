2bae0bc93e82e650a0574dcdb05802e52169e43d
Nur Rachmat <rachmat.nur91@gmail.com>
2023-12-01 17:38:59 +0700

    Classs Koneksi

---
+++ projectSekolahMDP/ClassKoneksi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectSekolahMDP
+{
+    class ClassKoneksi
+    {
+        //Koneksi SQL Server
+        public SqlConnection koneksi = new SqlConnection();
+
+        //perintah SQL
+        public SqlCommand perintahSQL = new SqlCommand();
+
+        //Data Adapter = mengisi data ke datagridview
+        public SqlDataAdapter da = new SqlDataAdapter();
+
+        //Data Set = Tabel
+        public DataSet ds = new DataSet();
+
+        //Data Reader = pencarian data ke tabel
+        public SqlDataReader cari;
+
+        public void bukaDatabase()
+        {
+            try
+            {
+                //Jika DataBase Berhasil Diakses
+                koneksi.ConnectionString = "Data Source=.;Initial Catalog=dbMDPSchool;Integrated Security=True";
+                koneksi.Open();
+                perintahSQL.Connection = koneksi;
+            }
+            catch
+            {
+                //Jika Database Tidak Berhasil Diakses
+                MessageBox.Show("Koneksi Database Gagal", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void tutupDatabase()
+        {
+            koneksi.Close();
+        }
+
+    }
+}
--- projectSekolahMDP/FormLogin.cs
+++ projectSekolahMDP/FormLogin.cs
@@ -12,18 +12,62 @@ namespace projectSekolahMDP
 {
     public partial class FormLogin : Form
     {
-        public static String username = "";
-        public static String level = "";
+        public static string username = "";
+        public static string level = "";
+
+        ClassKoneksi koneksi = new ClassKoneksi();
+
         public FormLogin()
         {
             InitializeComponent();
             bersih();
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        //Tombol Login
+        private void btnLogin_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            if (txtUsername.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Username / Password Harus Diisi", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                bersih();
+            }
+            else
+            {
+                koneksi.bukaDatabase();
+
+                //perintah sql untuk menampilkan data pada tabel Users berdasarkan username dan password yang diinputkan
+                koneksi.perintahSQL.CommandText = "SELECT * FROM Users WHERE username = @user AND password = @pass";
+
+                koneksi.perintahSQL.Parameters.AddWithValue("@user", txtUsername.Text);
+                koneksi.perintahSQL.Parameters.AddWithValue("@pass", txtPassword.Text);
+
+                koneksi.cari = koneksi.perintahSQL.ExecuteReader();
+
+                if (koneksi.cari.Read() == true)
+                {
+                    //jika data ada pada tabel Users di database, maka tampilkan pesan Selamat <<username>> Anda Berhasil Login sebagai <<level>>
+                    username = koneksi.cari["username"].ToString();
+                    level = koneksi.cari["level"].ToString();
+
+                    MessageBox.Show("Selamat " + username + ", Anda Berhasil Login \nSebagai " + level, "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    this.Hide();
+
+                    FormMenuUtama menuUtama = new FormMenuUtama();
+                    menuUtama.Show();
+                }
+                else
+                {
+                    //jika data tidak ada pada tabel Users maka akan menampilkan pesan Username / Password salah
+                    MessageBox.Show("Username / Password Salah", "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bersih();
+                }
+
+                koneksi.tutupDatabase();
+            }
         }
+
+
 
         public void bersih()
         {
@@ -32,17 +76,10 @@ namespace projectSekolahMDP
             txtUsername.Focus(); //agar kursor fokus ke teksbox
         }
 
-        //Tombol Login
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnCancel_Click(object sender, EventArgs e)
         {
-            //Set variabel username dan level
-            username = "Mimin";
-            level = "admin";
-
-            FormMenuUtama menuUtama = new FormMenuUtama();
-            menuUtama.Show(); //tampilkan menu utama
-
-            this.Hide();    //menyembunyikan form
+            Application.Exit();
         }
+
     }
 }
